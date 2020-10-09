c55f643b7efd1b96512f3b474e57887b9c3916ac
avk1994 <a.v.k.1994@outlook.com>
2020-10-09 22:50:33 +0300

    structure.java added

---
+++ src/structure.java
@@ -0,0 +1,15 @@
+public class structure {
+    String id;
+    String Name;
+    String Url;
+    int freeMinutes;
+    String unlimitedMinutesForTele2;
+    int freeGbInternet;
+    String unlimitedGbSocialNetwork;
+    boolean bestseller;
+    int priceForMonth;
+    String buy;
+    String buyUrl;
+    boolean customization;
+    String customizationUrl;
+}
